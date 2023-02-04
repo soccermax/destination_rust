257b2e644a50aad3e9b8d093891837b60ac5ed09
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2023-02-04 10:19:26 +0100

    cleanup

--- src/db/destination.rs
+++ src/db/destination.rs
@@ -11,23 +11,16 @@ use crate::db::error;
 
 pub fn create_destination(mut new_destination: Destination)
                           -> Result<Destination, error::DbError> {
-    let mut connection = client::create_client()?;
-
+    let mut connection = create_client()?;
     let mut all_destinations = get_all()?;
-    println!("positive request: {}", all_destinations);
-
     let existing_destination = &all_destinations[&new_destination.name];
-    println!("existing destination: {}", existing_destination);
-
     if serde_json::Value::is_null(existing_destination) {
         new_destination.id = Some(Uuid::new_v4().to_string());
         all_destinations[&new_destination.name] = serde_json::to_value(&new_destination).unwrap();
     } else {
        return Err(error::DbError::AlreadyExists {name: new_destination.name});
     }
-
     connection.set("DESTINATION", all_destinations.to_string())?;
-
     Ok(Destination {
         id: None,
         authentication: Some(Authentication::BasicAuth),
@@ -48,54 +41,3 @@ pub fn get_all() -> Result<serde_json::Value, error::DbError> {
     let string: String = redis::from_redis_value(&all_destinations).unwrap();
     Ok(serde_json::from_str(&string).unwrap())
 }
-
-
-// use std::error::Error;
-// use serde::{Deserialize, Serialize};
-// use serde_json;
-// use redis;
-// use redis::{ConnectionLike, RedisError, RedisResult};
-// use redis::Commands;
-//
-//
-// #[derive(Serialize, Deserialize)]
-// struct Destination {
-//     id: String,
-//     name: String,
-//     port: u16
-// }
-//
-// fn main() {
-//     // Statements here are executed when the compiled binary is called
-//     let new_destination = Destination {
-//         id: String::from("id"),
-//         name: String::from("ER9"),
-//         port: 8080
-//     };
-//     let json_string = serde_json::to_string(&new_destination);
-//     // println!("serialized json: {}", &json_string.unwrap());
-//
-//     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
-//     let mut con = client.get_connection().unwrap();
-//
-//     let result = con.is_open();
-//     println!("is open: {}", result);
-//     let _:() = con.set(new_destination.id.to_string(), json_string.unwrap()).unwrap();
-//     // match result {
-//     //     Ok(value) => println!("positive: {}", "done"),
-//     //     Err(err) => panic!("panic")
-//     // }
-//
-//
-//     let value_get : Result<String, RedisError> = con.get("id");
-//     if value_get.is_ok() {
-//         let value : String = value_get.unwrap();
-//         println!("found the key and all good: {}", &value);
-//         let retrieved_destination : Destination = serde_json::from_str(&value).unwrap();
-//         println!("destination name is: {} with port {}", retrieved_destination.name,
-//                  retrieved_destination.port)
-//     } else {
-//         println!("key not found");
-//     }
-//
-// }
--- src/handler/error.rs
+++ src/handler/error.rs
@@ -30,9 +30,6 @@ impl IntoResponse for ApirError {
             ApirError::InternalServerError => {
                 (StatusCode::INTERNAL_SERVER_ERROR, String::from("Internal Server Error"))
             }
-            // AppError::UserRepo(UserRepoError::InvalidUsername) => {
-            //     (StatusCode::UNPROCESSABLE_ENTITY, "Invalid username")
-            // }
         };
         let body = Json(json!({
             "errorMessage": error_message,
@@ -40,40 +37,3 @@ impl IntoResponse for ApirError {
         (status, body).into_response()
     }
 }
-
-
-
-
-
-
-// use axum::{extract::rejection::JsonRejection, http::StatusCode, response::IntoResponse, Json};
-// use serde_json::{json};
-// use thiserror::Error;
-//
-//
-// // We derive `thiserror::Error`
-// #[derive(Debug, Error)]
-// pub enum ApiError {
-//     // The `#[from]` attribute generates `From<JsonRejection> for ApiError`
-//     // implementation. See `thiserror` docs for more information
-//     #[error(transparent)]
-//     JsonExtractorRejection(#[from] JsonRejection),
-// }
-// // We implement `IntoResponse` so ApiError can be used as a response
-// impl IntoResponse for ApiError {
-//     fn into_response(self) -> axum::response::Response {
-//         let payload = json!({
-//             "message": self.to_string(),
-//             "origin": "with_rejection"
-//         });
-//         let code = match self {
-//             ApiError::JsonExtractorRejection(x) => match x {
-//                 JsonRejection::JsonDataError(_) => StatusCode::UNPROCESSABLE_ENTITY,
-//                 JsonRejection::JsonSyntaxError(_) => StatusCode::BAD_REQUEST,
-//                 JsonRejection::MissingJsonContentType(_) => StatusCode::UNSUPPORTED_MEDIA_TYPE,
-//                 _ => StatusCode::INTERNAL_SERVER_ERROR,
-//             },
-//         };
-//         (code, Json(payload)).into_response()
-//     }
-// }
