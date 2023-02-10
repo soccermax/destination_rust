8fb26374e3f43fc1eb283bdb1d932e1824727b7d
Max Gruenfelder <maximilian.gruenfelder@sap.com>
2023-02-10 14:01:56 +0100

    best refactor

--- src/handler/destination.rs
+++ src/handler/destination.rs
@@ -4,58 +4,28 @@ use crate::db::destination;
 use crate::handler::error;
 use crate::model::destination::Destination;
 
-// basic handler that responds with a static string
-pub async fn root() -> &'static str {
-    "Hello, World!"
+pub async fn get_destinations() -> Result<impl IntoResponse, error::ApirError> {
+    let destinations = destination::get_all()?;
+    Ok((StatusCode::OK, Json(destinations)))
 }
 
 pub async fn create_destination(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
     Json(payload): Json<Destination>,
 ) -> Result<impl IntoResponse, error::ApirError> {
-    let destination = Destination {
-        id: None,
-        name: payload.name,
-        protocol: payload.protocol,
-        port: payload.port,
-        url: payload.url,
-        authentication: payload.authentication,
-    };
-
-    let create_des_result = destination::create_destination(destination);
-
-    match create_des_result {
-        Ok(new_destination) => Ok((StatusCode::CREATED, Json(new_destination))),
-        Err(err) => Err(err.into()),
-    }
+    let new_destination = destination::create_destination(payload)?;
+    Ok((StatusCode::CREATED, Json(new_destination)))
 }
 
 pub async fn get_destination(
     Path(destination_name): Path<String>,
 ) -> Result<impl IntoResponse, error::ApirError> {
-    let create_des_result = destination::get_destination(destination_name);
-
-    match create_des_result {
-        Ok(destination) => Ok((StatusCode::OK, Json(destination))),
-        Err(err) => Err(err.into()),
-    }
-}
-
-pub async fn get_destinations() -> Result<impl IntoResponse, error::ApirError> {
-    let create_des_result = destination::get_all();
-
-    match create_des_result {
-        Ok(destination) => Ok((StatusCode::OK, Json(destination))),
-        Err(err) => Err(err.into()),
-    }
+    let destination = destination::get_destination(destination_name)?;
+    Ok((StatusCode::CREATED, Json(destination)))
 }
 
 pub async fn delete_destination(
     Path(destination_name): Path<String>,
 ) -> Result<impl IntoResponse, error::ApirError> {
-    match destination::delete_destination(destination_name) {
-        Ok(_) => Ok(StatusCode::NO_CONTENT),
-        Err(err) => Err(err.into()),
-    }
+    destination::delete_destination(destination_name)?;
+    Ok(StatusCode::NO_CONTENT)
 }
--- src/router.rs
+++ src/router.rs
@@ -7,13 +7,12 @@ use axum::{
 pub fn initialize() -> Router {
     // build our application with a route
     Router::new()
-        .route("/", get(destination::root))
+        .route("/destination", get(destination::get_destinations))
         .route("/destination", post(destination::create_destination))
         .route(
             "/destination/:destination_name",
             get(destination::get_destination),
         )
-        .route("/destination", get(destination::get_destinations))
         .route(
             "/destination/:destination_name",
             delete(destination::delete_destination),
